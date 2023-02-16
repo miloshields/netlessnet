fd02478be81d2d0333794bb51c635f78d1cb88f4
miloshields <milo.shields@gmail.com>
2023-02-16 18:27:46 -0500

    add twilio env vars

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-console.log("Hello, world!")
+const { env } = require("process")
+require('dotenv').config();
+
+TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+TWILIO_AUTH_TOKEN  = process.env.TWILIO_AUTH_TOKEN
